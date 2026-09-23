5856d4db691588d83c96d4566b071b6247aa181e
ChromEvoRamanlab <chromevoramanlab@gmail.com>
2026-09-23 17:19:43 +0000

    Deploying to gh-pages from @ ChromEvoRamanlab/ChromEvoRamanlab.github.io@e0059129d423148ecbdcc389d28fe5f518f68da6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -31,7 +31,7 @@ ninja.data = [{
             window.location.href = "/publications/";
           },
         },{id: "nav-philosophy",
-          title: "Philosophy",
+          title: "philosophy",
           description: "",
           section: "Navigation",
           handler: () => {
