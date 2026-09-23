0ec3d031f403c13720673aa40e00af40ba7e926f
ChromEvoRamanlab <chromevoramanlab@gmail.com>
2026-09-23 17:19:55 +0000

    Deploying to gh-pages from @ ChromEvoRamanlab/ChromEvoRamanlab.github.io@c15c6cb2b1c498a5c891be59d3b8488306c904a1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -10,7 +10,7 @@ ninja.data = [{
       window.location.href = "/";
     },
   },{id: "nav-research",
-          title: "research",
+          title: "Research",
           description: "",
           section: "Navigation",
           handler: () => {
@@ -24,14 +24,14 @@ ninja.data = [{
             window.location.href = "/people/";
           },
         },{id: "nav-publications",
-          title: "publications",
+          title: "Publications",
           description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
           },
         },{id: "nav-philosophy",
-          title: "philosophy",
+          title: "Philosophy",
           description: "",
           section: "Navigation",
           handler: () => {
